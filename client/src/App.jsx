967a9bb47c3d04633d3d93b422900f065f8b8e99
nadine <n_hadjaissa@estin.dz>
2024-11-29 22:57:46 +0100

    homepagecreated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,19 +4,9 @@ import './App.css';
 import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-<<<<<<< HEAD
-      <Homepage /> {/* Render the Homepage component here */}
-=======
-    <div className='m-5 p-5'>
-
-      <p className=' text-pretty'>helll  </p>
-    </div>
->>>>>>> 5020242b7dc781589a62ccde9da3e52ab4419ef7
-    </>
+    <Homepage/>
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-=======
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
->>>>>>> 5020242b7dc781589a62ccde9da3e52ab4419ef7
   theme: {
     extend: {
       colors: {
