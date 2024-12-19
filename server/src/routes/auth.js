2626e15379435fca6a41f6b15b92cae20c8c05e4
ziyad <aldjahmaniziad@gmail.com>
2024-12-19 23:11:22 +0100

    registerteacher

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,6 +3,9 @@ const { login, register, logout } = require('../controllers/auth')
 const {Router} = require("express")
 const { loginValidation, registerValidation } = require('../validators/auth')
 const { validationMiddleware } = require('../middlewares/validators-middleware')
+const { registerTeacher } = require('../controllers/teacherController');
+const { approveOrRejectTeacher } = require('../controllers/adminController');
+
 
 const router = Router()
 
@@ -12,6 +15,13 @@ router.post('/register', registerValidation, validationMiddleware, register)
 router.post('/login', loginValidation, validationMiddleware, login)
 
 router.get('/logout', logout)
+
+router.post('/registerteacher' ,validationMiddleware,registerTeacher);
+
+router.put('/admin/approveteacher/:teacher_id',registerTeacher, adminAuth, approveOrRejectTeacher);
+
+
+
 
 
 
